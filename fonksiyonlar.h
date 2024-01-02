1789aa23e3afa7737624d83a6efee7f36d92e93d
mertsemih <147096231+mertsemih@users.noreply.github.com>
2024-01-02 21:11:29 +0300

    fonksiyonlar.h
    fonksiyonlar adındaki kutuphanemizin dosyası

---
+++ fonksiyonlar.h
@@ -0,0 +1,7 @@
+void cikarkitap();
+void eklekitap();
+void kitapgorunumu();
+void arakitap();
+void kitapDuzenle();
+void kitaplariDosyayaYaz();
+//Burada kutuphanemizi olusturup,yazdıgımız fonksiyonları tanımladık.
